c61ec4d1576f6e2dd1908026b0e360ad63b60ee7
Thomas Vargas <rodrigues.pauloricardo@gmail.com>
2021-07-18 20:42:15 -0300

    att

---
+++ botao.js
@@ -0,0 +1,10 @@
+function validar(){
+  let botao = document.getElementById("button");
+  let email = document.getElementById("email");  
+
+
+ 
+    res.innerHTML = ("Você Ganhou um Cupom extra de 5% :) Aguarde as promoções no seu email");
+  }
+
+  
---
+++ site.js
@@ -0,0 +1,13 @@
+import Countdown from "./countdown.js";
+
+const tempoParaOWWB = new Countdown("31 Julho 2021 20:00:00 GMT-0300");
+const tempos = document.querySelectorAll("[data-time]");
+
+function mostrarTempo() {
+  tempos.forEach((tempo, index) => {
+    tempo.innerHTML = tempoParaOWWB.total[index];
+  });
+}
+mostrarTempo();
+setInterval(mostrarTempo, 1000);
+
