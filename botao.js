2173719269407014c56a684f068dcd1e4261ed74
Thomas Vargas <rodrigues.pauloricardo@gmail.com>
2021-07-22 20:23:01 -0300

    att

--- botao.js
+++ botao.js
@@ -1,13 +1,13 @@
-  function validar(){
+function validar() {
 
 
-    let email =     document.getElementById("email")
-    let botao =     document.getElementById("button")
-  
-    if (email.value == ""){
+  let email = document.getElementById("email")
+  let botao = document.getElementById("button")
+
+  if (email.value == "") {
       res.innerHTML = ("Digite um email válido")
-  }else {
-    res.innerHTML = ("Você Ganhou um Cupom extra de 5% :) Aguarde as promoções no seu email");
-  
+  } else {
+      res.innerHTML = ("Você Ganhou um Cupom extra de 5% :) Aguarde as promoções no seu email");
+
   }
-  }
+}
